64321978da84bd4331c0db82fe9c89a32a362bbe
Juan Carlos Vergara <juancavergara6@gmail.com>
2023-02-09 11:21:59 +0100

    Actualización de los métodos bmi y category

--- src/main/java/bmicalc/BMICalcImpl.java
+++ src/main/java/bmicalc/BMICalcImpl.java
@@ -3,32 +3,33 @@ package bmicalc;
 public class BMICalcImpl implements BMICalc {
 
 	public double bmi(double mass, double height) {
-		
-		return mass / (height * height);
+		if(mass>0 && height>0)
+			return mass / (height * height);
+		else return 0;
 	}
 
 	public String category(double bmi) {
+		if (bmi<=0) return "ERROR: El BMI debe ser mayor que 0";
 		
-		
-		if(bmi<18.5) return "UNDERWEIGHT";
+		else if(bmi<18.5) return "UNDERWEIGHT";
 		else if (bmi<=24.9) return "NORMAL";
 		else if (bmi<=29.9) return "OVERWEIGHT";
 		else return "OBESE";
 
 	}
 	/**
-	 * Categorize a person according to the BMI category:
-	 *   - UNDERWEIGHT for a BMI below 18.5
-	 *   - NORMAL for a BMI between 18.5 and 24.9
-	 *   - OVERWEIGHT for a BMI between 25.0 and 29.9
-	 *   - OBESE for a BMI above or equal 30.0
-	 *   
-	 * @param bmi Body Mass Index (BMI) value.
-	 * @return	  String representing the BMI category.
+	 * Determine whether a person has abdominal obesity.
+	 * A woman has abdominal obesity if her waist circumference is greater than 80 cm.
+	 * A man has abdominal obesity if his waist circumference is greater than 90 cm.
+	 * 
+	 * @param waistCircumference	Waist circumference.
+	 * @param gender				Male ('M') or Female ('F').
+	 * @return						True if the person has abdominal obesity, false otherwise.
 	 */
-
 	public boolean abdominalObesity(double waistCircumference, char gender) {
-		return false;
+		
+		 return false;
+		
 	}
 
 }
