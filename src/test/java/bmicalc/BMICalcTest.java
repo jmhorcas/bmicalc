64c40b1687510d0638ae8396c7828cace545836f
PabloB2G <pablobermudezgamez@uma.es>
2023-02-10 17:35:24 +0100

    4 tests for bmi method

--- src/test/java/bmicalc/BMICalcTest.java
+++ src/test/java/bmicalc/BMICalcTest.java
@@ -2,14 +2,41 @@ package bmicalc;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class BMICalcTest {
+	BMICalcImpl calc;
 	
-	@Test
-	@DisplayName("Example Test Method.")
-	public void bmi() {
-		assertEquals(true, true);
+	@BeforeAll
+	public void loadCalc() {
+		calc = new BMICalcImpl();
 	}
+	
+	@ParameterizedTest
+    @ValueSource(doubles = {1.5, 2.0, 3.2, 4.0, 10.5})
+	public void bmiPositiveReals(double value) {
+		assertEquals(value / Math.sqrt(value*value), calc.bmi(value, value));
+	}
+	
+	@ParameterizedTest
+    @ValueSource(ints = {1, 2, 3, 4, 10})
+	public void bmiPositiveInts(double value) {
+		assertEquals(value / (value*value), calc.bmi(value, value));
+	}
+	
+	@ParameterizedTest
+    @ValueSource(doubles = {-1.5, -2.0, -3.2, -4.0, -10.5})
+	public void bmiNegativeReals(double value) {
+		assertEquals(value / (value*value), calc.bmi(value, value));
+	}
+	
+	@ParameterizedTest
+    @ValueSource(ints = {-1, -2, -3, -4, -10})
+	public void bmiNegativeInts(double value) {
+		assertEquals(value / (value*value), calc.bmi(value, value));
+	}
+	
+	
 }
