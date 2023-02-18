45280086747d97d09a22f88f6e8bb5d8f468264d
ElArquitectorgo <mega2814s@gmail.com>
2023-02-18 11:33:59 +0100

    Category method tests

--- README.md
+++ README.md
@@ -3,4 +3,11 @@
 # BMI calculator
 A biomedical calculator for the body mass index (BMI) and other parameters.
 
+# Tests
+
+underweightCategoryTest: comprueba con valores en rango que el resultado es correcto.
+underweightNegativeCategoryTest: comprueba que con valores negativos el resultado es de error.
+normalCategoryTest: comprueba con valores en rango que el resultado es correcto.
+overweightCategoryTest: comprueba con valores en rango que el resultado es correcto.
+obeseCategoryTest: comprueba con valores en rango que el resultado es correcto.
 
--- src/test/java/bmicalc/BMICalcTest.java
+++ src/test/java/bmicalc/BMICalcTest.java
@@ -12,4 +12,58 @@ public class BMICalcTest {
 	public void bmi() {
 		assertEquals(true, true);
 	}
+
+	/*   - underweight for a bmi below 18.5
+	 *   - normal for a bmi between 18.5 and 24.9
+	 *   - overweight for a bmi between 25.0 and 29.9
+	 *   - obese for a bmi above or equal 30.0
+	 * */
+ 
+	@Test
+	public void underweightCategoryTest() {
+		BMICalcImpl calc = new BMICalcImpl();
+		double[] nums = {0, 15, 18.49};
+		for (int i = 0; i < nums.length; i++) {		
+			String category = calc.category(nums[i]);
+			assertEquals(category, "underweight");
+		}
+	}
+
+	@Test
+	public void underweightNegativeCategoryTest() {
+		BMICalcImpl calc = new BMICalcImpl();	
+		String category = calc.category(-5);
+		assertEquals(category, "Error: negative input");
+	}
+
+	@Test
+	public void normalCategoryTest() {
+		BMICalcImpl calc = new BMICalcImpl();
+		double[] nums = {18.5, 20, 24.9, 24.98};
+		for (int i = 0; i < nums.length; i++) {		
+			String category = calc.category(nums[i]);
+			assertEquals(category, "normal");
+		}
+	}
+
+	@Test
+	public void overweightCategoryTest() {
+		BMICalcImpl calc = new BMICalcImpl();
+		double[] nums = {25.0, 27.7, 29.9, 29.95};
+		for (int i = 0; i < nums.length; i++) {		
+			String category = calc.category(nums[i]);
+			assertEquals(category, "overweight");
+		}
+	}
+
+	@Test
+	public void obeseCategoryTest() {
+		BMICalcImpl calc = new BMICalcImpl();
+		double[] nums = {30, 40, 50};
+		for (int i = 0; i < nums.length; i++) {		
+			String category = calc.category(nums[i]);
+			assertEquals(category, "obese");
+		}
+	}
+	
 }
