52c2cb26b6f2bbca7cba5e527e455062fa7d84f6
ElArquitectorgo <mega2814s@gmail.com>
2023-02-18 11:55:51 +0100

    Implemented bmi method

--- src/main/java/bmicalc/BMICalcImpl.java
+++ src/main/java/bmicalc/BMICalcImpl.java
@@ -3,7 +3,7 @@ package bmicalc;
 public class BMICalcImpl implements BMICalc {
 
 	public double bmi(double mass, double height) {
-		return 0.0;
+		return Math.abs(mass) / Math.pow(height, 2);
 	}
 
 	public String category(double bmi) {
