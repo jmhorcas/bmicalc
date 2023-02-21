555b481b5ca055cb462702801785490465635d61
Juan Carlos Vergara <juancavergara6@gmail.com>
2023-02-21 17:33:05 +0100

    Corrección de errores

--- src/main/java/bmicalc/BMICalcImpl.java
+++ src/main/java/bmicalc/BMICalcImpl.java
@@ -2,14 +2,14 @@ package bmicalc;
 
 public class BMICalcImpl implements BMICalc {
 
-	public double bmi(double mass, double height) {
+	public double bmi(double mass, double height) throws ArithmeticException {
 		if(mass>0 && height>0)
 			return mass / (height * height);
-		else return 0;
+		else throw new ArithmeticException();
 	}
 
 	public String category(double bmi) {
-		if (bmi<=0) return "ERROR: El BMI debe ser mayor que 0";
+		if (bmi<=0) return "ERROR";
 		
 		else if(bmi<18.5) return "UNDERWEIGHT";
 		else if (bmi<=24.9) return "NORMAL";
@@ -17,19 +17,22 @@ public class BMICalcImpl implements BMICalc {
 		else return "OBESE";
 
 	}
-	public boolean abdominalObesity(double waistCircumference, char gender) {
+	public boolean abdominalObesity(double waistCircumference, char gender) throws RuntimeException{
 		
-		switch(gender) {
-		case 'M' :
-			if (waistCircumference>90) return true;				
-			break;
-		case 'F' :
-			if (waistCircumference>80) return true;
-			break;
-		default:
-			System.out.println("Debe indicar el género con F o M");
-				
-	 }
+		
+		if(waistCircumference <= 0 ) throw new RuntimeException("La circunferencia debe ser mayor que 0");
+		else switch(gender) {
+				case 'M' :
+					if (waistCircumference>90) return true;				
+					break;
+				case 'F' :
+					if (waistCircumference>80) return true;
+					break;
+				default:
+					//System.out.println("Debe indicar el género con F o M");	
+					throw new RuntimeException("Debe indicar el género con F o M");
+			}
+		
 	 return false;
 		
 		
